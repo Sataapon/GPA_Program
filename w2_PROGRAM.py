9ed1153ea32852ebf58d74a3bb9a0b47728e1752
Satapaon <mr.sataapon@gmail.com>
2019-01-10 17:05:29 +0700

    Refactor program "Useful variable name"

--- w2_PROGRAM.py
+++ w2_PROGRAM.py
@@ -1,22 +1,22 @@
-def get_subjects(file_name):
+def set_subjects(file_name):
     file = open(file_name)
     subjects = file.read().splitlines()
     file.close()
     return subjects
 
-def save(subjects_studied, file_name):
+def save(subjects, file_name):
     file = open(file_name, 'w')
-    for item in subjects_studied:
+    for item in subjects:
         file.write(f'{item}\n')
     file.close
 
-def insert(subjects_studied, new):
-    subjects_studied.append(new)
+def insert(subjects, new):
+    subjects.append(new)
 
-def edit(subjects_studied, old, new):
-    for i in range(len(subjects_studied)):
-        if subjects_studied[i] == old:
-            subjects_studied[i]= new
+def edit(subjects, old, new):
+    for i in range(len(subjects)):
+        if subjects[i] == old:
+            subjects[i]= new
 
 def check_command(cmd_arg):
     if cmd_arg[0] == 'open' and len(cmd_arg) == 2:
@@ -32,7 +32,7 @@ def check_command(cmd_arg):
 
 #--------------------------------------------------#
 
-subjects_studied = None
+subjects = None
 file_name = None
 
 while True:
@@ -41,13 +41,13 @@ while True:
 
     in_command = check_command(cmd_arg)
     if in_command == 'open':
-        subjects_studied = get_subjects(cmd_arg[1])
+        subjects = set_subjects(cmd_arg[1])
         file_name = cmd_arg[1]
     elif in_command == 'save' and file_name:
-        save(subjects_studied, file_name)
-    elif in_command == 'insert' and subjects_studied:
-        insert(subjects_studied, cmd_arg[1])
-    elif in_command == 'edit' and subjects_studied:
-        edit(subjects_studied, cmd_arg[1], cmd_arg[2])
+        save(subjects, file_name)
+    elif in_command == 'insert' and subjects:
+        insert(subjects, cmd_arg[1])
+    elif in_command == 'edit' and subjects:
+        edit(subjects, cmd_arg[1], cmd_arg[2])
     elif in_command == 'exit':
         break
